3524ea5599b85a749f65f58c5ef3ba27b771d628
chrissan <christian-3158@comunidad.unam.mx>
2018-09-18 10:40:27 -0500

    LUNES4

--- Lunes/Main.java
+++ Lunes/Main.java
@@ -1,8 +1,9 @@
 public class Main{
 	public static void main (String[] args){
 		//Persona p1 = new Persona();//uandeclaracion de objeto siempre despues de la palabra new
-		tarea1.producto t1p1 = new tarea1.producto("leche",10);
-		tarea1.empleado t1e1 = new tarea1.empleado("j",20);
+		tarea1 tienda_uno = new tarea1("ermita", 15);
+		tarea1.producto tienda_unop1 = new tarea1.producto("leche",10);
+		
 		//Persona p2 = new Persona(25,"raul");
 
 		//p1.edad=25 mala practica
