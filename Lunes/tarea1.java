b8977668f9fcc167405a528ac4207c5e6e993b7d
chrissan <christian-3158@comunidad.unam.mx>
2018-09-18 09:22:33 -0500

    LUNES1

---
+++ Lunes/tarea1.java
@@ -0,0 +1,22 @@
+public class tarea1 {
+	private String direccion;
+	private float dinero;
+	private int cont_prod
+	private int cont_empl
+
+	public class producto
+	{
+		public String nombre_prod;
+		public int precio_prod;
+	}
+
+	public class empleado
+	{
+		public String nombre_emp;
+		public int precio_emp;
+	}
+
+
+
+
+}
