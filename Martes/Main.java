42b60beff349d109a2c124f0c288818c69a2604c
chrissan <christian-3158@comunidad.unam.mx>
2018-09-19 08:13:06 -0500

    NSQH2

---
+++ Martes/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("---->>>>>Bienvenido a su calcu<<<<<-----");
+		Scanner entrada = new Scanner(System.in);
+		System.out.println("Digite el NUMERO deseado:");
+		System.out.println("1.SUMA");
+		System.out.println("2.RESTA");
+		System.out.println("3.MULTIPLICACION");
+		System.out.println("4.DIVISION");
+		int n;
+		n = entrada.nextInt();
+	}
+	
+}
